a658ea9a7401f019f8e3eea85e888cbf06403280
Vitoria Faria <vitoria.faria@repassa.com.br>
2024-01-10 14:49:39 -0300

    feat(26139): corrige declaração de metodo

--- lib/seller_center_integration/client.rb
+++ lib/seller_center_integration/client.rb
@@ -22,9 +22,5 @@ module SellerCenterIntegration
     def http_client
       HttpClient.new(@options)
     end
-
-    def get_product_price_by_gender(params)
-      @service.get_product_price_by_gender(params)
-    end
   end
 end
